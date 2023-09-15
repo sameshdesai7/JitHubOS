19f55d43c408663aada507abbf86d8bc723f739a
Noah Marner <ndm00004@mix.wvu.edu>
2023-09-15 14:08:47 -0400

    Added functions in pcb.c file

--- kernel/pcb.c
+++ kernel/pcb.c
@@ -1,4 +1,26 @@
 #include <pcb.h>
 
+pcb* pcb_allocate(void){
 
+}
+
+int pcb_free(pcb* pcb){
+
+}
+
+pcb* pcb_setup(const char * name, int clas, int priority){
+
+}
+
+pcb* pcb_find(const char * name){
+
+}
+
+void pcb_insert(pcb* pcb){
+
+}
+
+void pcb_remove(pcb* pcb){
+
+}
 
