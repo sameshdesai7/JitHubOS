19c523c84ec285e21652a3b141c4046a93b6af7d
root <root@LAPTOP-U0TRDE4I>
2023-10-11 13:24:17 -0400

    Wrote sys_call() function and assembly for sys_call_isr

--- kernel/sys_call.c
+++ kernel/sys_call.c
@@ -4,9 +4,45 @@
 #include <sys_req.h>
 #include <dataStructs.h>
 
+pcb* cop = NULL;
+context* cop_context = NULL;
+
 context* sys_call(context* proc_context) {
     sys_req(IDLE);
 
+    if (cop_context == NULL) {
+        cop_context = proc_context;
+    }
 
-    return dequeue(ready)->stack_ptr;
+    int EAX = proc_context->EAX;
+    
+    //If EAX is IDLE, meaning the process is only giving up control of the CPU for the time being, we save the context of the process and put it back in the ready queue
+    if (EAX == IDLE) {
+        pcb* temp = dequeue(ready);
+        if (temp == NULL) {
+            return cop_context;
+        }
+        else {
+            cop->stack_ptr = cop_context;
+            cop->state = "ready";
+            enqueue(cop);
+            cop = temp;
+            cop->state = "running";
+            cop_context = temp->stack_ptr;
+            return cop_context;
+        }
+    }
+
+    //If EAX is EXIT, meaning the process is terminating, we simply load in the next process
+    else if (EAX == EXIT) {
+        pcb* temp = dequeue(ready);
+        if (temp == NULL) {
+            return cop_context;
+        }
+        else {
+            cop = temp;
+            cop_context = temp->stack_ptr;
+            return cop_context;
+        }
+    }
 }
--- kernel/sys_call_isr.s
+++ kernel/sys_call_isr.s
@@ -4,5 +4,31 @@ global sys_call_isr
 ;;; System call interrupt handler. To be implemented in Module R3.
 extern sys_call			; The C function that sys_call_isr will call
 sys_call_isr:
-	mov eax, -1		;; DELETE THIS LINE WHEN YOU IMPLEMENT R3
+	push eax
+	push ecx
+	push edx
+	push ebx
+	push ebp
+	push esi
+	push edi
+	push ss
+	push ds
+	push es
+	push fs
+	push gs
+	push esp
+	call sys_call
+	mov esp, eax
+	pop gs
+	pop fs
+	pop es
+	pop ds
+	pop ss
+	pop edi
+	pop esi
+	pop ebp
+	pop ebx
+	pop edx
+	pop ecx
+	pop eax
 	iret
